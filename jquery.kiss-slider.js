c56d1490fc17dc7a97899754ab7d9030dca876e7
Vivien <vivienleneez@gmail.com>
2014-12-09 18:35:53 +0100

    wip

---
+++ jquery.kiss-slider.js
@@ -0,0 +1,11 @@
+(function ( $ ) {
+	$.fn.kissSlider = function(options) {
+		var defaults = {
+			
+		};
+		var settings = $.extend(defaults, options);
+
+		// return element for chaining
+		return this;
+	}
+}(jQuery));
